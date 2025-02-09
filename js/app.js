9bd40f0d3f7ca75f087fa7c672766cfcc1554586
Bruno Barbosa <vieirabruno153@gmail.com>
2025-02-09 18:57:25 -0300

    function confirmação de devolucao e function quantidade de alugueis

--- js/app.js
+++ js/app.js
@@ -6,13 +6,30 @@ function alterarStatus(id) {
 
 
     if (imagem.classList.contains("dashboard__item__img--rented")) {
+        let devolve = prompt ('Você deseja realmente devolver? digite 1 para sim ou 2 para não');
+        if (devolve == 1) {       
         imagem.classList.remove("dashboard__item__img--rented"); 
         botao.classList.remove("dashboard__item__button--return"); 
         botao.textContent = 'Alugar';
-
+        quantidadeAlugados();
+    }else{
+        quantidadeAlugados();
+        return;
+    }    
     }else{
         imagem.classList.add("dashboard__item__img--rented"); 
         botao.classList.add("dashboard__item__button--return"); 
         botao.textContent = 'Devolver';
+        quantidadeAlugados();
     }   
     }
+
+function quantidadeAlugados(jogos, palavaraJogo) {
+    jogos = document.getElementsByClassName("dashboard__item__img--rented").length;
+    if (jogos > 1){
+        palavaraJogo = "jogos alugados"
+    }else{
+        palavaraJogo = "jogo alugado"
+    }
+    alert ('você possui ' + jogos + ' ' + palavaraJogo);
+}
